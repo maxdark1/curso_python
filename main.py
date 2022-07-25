6de0986000effcd8baba8d9800bf3b9559540dc6
Juan Lara <max_lara3005@hotmail.com>
2022-07-25 10:34:08 -0500

    Primer commit curso de Python

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Hola Mundo con Python")
+
+miVariable = "Hola Mundo desde una variable"
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+miVariable = 3005
+print(miVariable)
+print(miVariable)
+print(miVariable)
+
+x = 10
+y = 2
+z = x + y
+print(z)
+print(id(z)) #la funcion id nos regresa la direccion de memoria donde se encuentra guardada el valor de z
+
+nombre = "Juan Lara"
+telefono = 8136956121
+email = "max_lara3005@hotmail.com"
+print(nombre)
+print(telefono)
+print(email)
